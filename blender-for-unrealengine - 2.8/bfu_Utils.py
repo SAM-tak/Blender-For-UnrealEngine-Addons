251c20652e27cc55fad4a8057f781e6f54c09aaa
SAM-tak <osamu.takasugi@gmail.com>
2020-10-18 21:56:20 +0900

    fix typo

--- blender-for-unrealengine - 2.8/bfu_Utils.py
+++ blender-for-unrealengine - 2.8/bfu_Utils.py
@@ -927,7 +927,7 @@ def UpdateUnrealPotentialError():
 			if Asset.obj not in objToCheck:
 				objToCheck.append(Asset.obj)
 			for child in GetExportDesiredChilds(Asset.obj):
-				if child not in objToCheck and not (Asset.obj.type == 'ARMATURE' and child.hide):
+				if child not in objToCheck and not (Asset.obj.type == 'ARMATURE' and not child.hide_viewport):
 					objToCheck.append(child)
 
 	MeshTypeToCheck = []
