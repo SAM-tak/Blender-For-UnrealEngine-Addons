5eaf1af2a759b46044e35e9189305e6301b4961f
BleuRaven <xavierloux.loux@gmail.com>
2024-11-13 16:22:31 +0100

    Avoid script fail when fail to read version

--- blender-for-unrealengine/bfu_addon_parts/bfu_object_ui_and_props.py
+++ blender-for-unrealengine/bfu_addon_parts/bfu_object_ui_and_props.py
@@ -1140,7 +1140,7 @@ class BFU_PT_BlenderForUnrealObject(bpy.types.Panel):
         layout = self.layout
         
         if bpy.app.version >= (4, 2, 0):
-            version_str = 'Version '+ bbpl.blender_extension.extension_utils.get_package_version()
+            version_str = 'Version '+ str(bbpl.blender_extension.extension_utils.get_package_version())
         else:
             version_str = 'Version '+ bbpl.blender_addon.addon_utils.get_addon_version_str("Unreal Engine Assets Exporter")
 
--- blender-for-unrealengine/bfu_write_utils.py
+++ blender-for-unrealengine/bfu_write_utils.py
@@ -71,7 +71,7 @@ def add_generated_json_meta_data(json_data):
     blender_file_path = bpy.data.filepath
 
     if bpy.app.version >= (4, 2, 0):
-        version_str = 'Version '+ bbpl.blender_extension.extension_utils.get_package_version()
+        version_str = 'Version '+ str(bbpl.blender_extension.extension_utils.get_package_version())
         addon_path = bbpl.blender_extension.extension_utils.get_package_path()
     else:
         version_str = 'Version '+ bbpl.blender_addon.addon_utils.get_addon_version_str("Unreal Engine Assets Exporter")
