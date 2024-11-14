2a521d8d3857ee6947e5ba24d5c8f27467cdd7f4
BleuRaven <xavierloux.loux@gmail.com>
2024-11-14 01:16:26 +0100

    Update Panel script names

--- blender-for-unrealengine/bfu_addon_parts/__init__.py
+++ blender-for-unrealengine/bfu_addon_parts/__init__.py
@@ -2,25 +2,25 @@ import bpy
 import importlib
 
 from . import bfu_modular_skeletal_specified_parts_meshs
-from . import bfu_object_ui_and_props
-from . import bfu_tool_ui_and_props
-from . import bfu_export_ui_and_props
+from . import bfu_panel_object
+from . import bfu_panel_tools
+from . import bfu_panel_export
 from . import bfu_export_correct_and_improv_panel
-from . import bfu_debug_ui_and_props_panel
+from . import bfu_debug_panel
 
 
 if "bfu_modular_skeletal_specified_parts_meshs" in locals():
     importlib.reload(bfu_modular_skeletal_specified_parts_meshs)
-if "bfu_object_ui_and_props" in locals():
-    importlib.reload(bfu_object_ui_and_props)
-if "bfu_tool_ui_and_props" in locals():
-    importlib.reload(bfu_tool_ui_and_props)
-if "bfu_export_ui_and_props" in locals():
-    importlib.reload(bfu_export_ui_and_props)
+if "bfu_panel_object" in locals():
+    importlib.reload(bfu_panel_object)
+if "bfu_panel_tools" in locals():
+    importlib.reload(bfu_panel_tools)
+if "bfu_panel_export" in locals():
+    importlib.reload(bfu_panel_export)
 if "bfu_export_correct_and_improv_panel" in locals():
     importlib.reload(bfu_export_correct_and_improv_panel)
-if "bfu_debug_ui_and_props_panel" in locals():
-    importlib.reload(bfu_debug_ui_and_props_panel)
+if "bfu_debug_panel" in locals():
+    importlib.reload(bfu_debug_panel)
 
 classes = (
 )
@@ -31,19 +31,19 @@ def register():
         bpy.utils.register_class(cls)
 
     bfu_modular_skeletal_specified_parts_meshs.register()
-    bfu_object_ui_and_props.register()
-    bfu_tool_ui_and_props.register()
-    bfu_export_ui_and_props.register()
+    bfu_panel_object.register()
+    bfu_panel_tools.register()
+    bfu_panel_export.register()
     bfu_export_correct_and_improv_panel.register()
-    bfu_debug_ui_and_props_panel.register()
+    bfu_debug_panel.register()
 
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
 
     bfu_modular_skeletal_specified_parts_meshs.unregister()
-    bfu_object_ui_and_props.unregister()
-    bfu_tool_ui_and_props.unregister()
-    bfu_export_ui_and_props.unregister()
+    bfu_panel_object.unregister()
+    bfu_panel_tools.unregister()
+    bfu_panel_export.unregister()
     bfu_export_correct_and_improv_panel.unregister()
-    bfu_debug_ui_and_props_panel.unregister()
+    bfu_debug_panel.unregister()
